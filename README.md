22ac9a106ebee9bf56b639b0bb0c27cce332a638
maryn <maryn@telepathics.xyz>
2019-01-26 11:16:11 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# 404
+> NOTHING TO SEE HERE (yet)
+
+Just basic files for projects in progress, can also be used as a quick 404.
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>UNDER CONSTRUCTION</title>
+	<link rel="stylesheet" href="./style.css?<?= date('his'); ?>">
+</head>
+<body>
+
+	<div id="left">
+		<h1>NOTHING TO SEE HERE (yet)</h1>
+		<p>feel free to check me out elsewhere</p>
+		<p><a href="https://telepathics.xyz">telepathics</a> | <a href="https://maryn.xyz">maryn</a> | <a href="https://revertdata.xyz">revertdata</a></p>
+	</div>
+	<img src="./ghost.gif" alt="ghost">
+	
+</body>
+</html>
